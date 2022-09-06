fbdddab6d03d16de44b104aea8da9a144c6de4b8
Henrique Poleselo <hpoleselo@gmail.com>
2022-09-06 00:04:36 -0300

    Adapted endpoint to use GCS.

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from matplotlib import image
 from pydantic import BaseModel
 from keras.models import load_model
 from utils.predict import predict_digit
+from utils.gcs_interface import read_from_gcs, write_to_gcs
 
 # Loading the model
 model = load_model('weights/best.h5')
@@ -14,7 +15,6 @@ app = FastAPI()
 async def main():
     return "Welcome to the API Demo!"
 
-
 @app.post('/upload/')
 async def create_upload_file(file : UploadFile = File(default=None)):
     file_location = f'images/{file.filename}'
@@ -24,6 +24,10 @@ async def create_upload_file(file : UploadFile = File(default=None)):
         f.write(file.file.read())
     return {"info": f'file {file.filename} saved at {file_location}'}
 
+@app.post('/upload_to_gcs/')
+async def create_upload_file(file : UploadFile = File(default=None)):
+    bucket_id = write_to_gcs(file.filename)
+    return {"file_location": bucket_id}
 
 @app.get('/predicted_image')
 async def predict(img_name : str):
@@ -34,4 +38,4 @@ async def predict(img_name : str):
 
     pred = predict_digit(model, img_name)
 
-    return {img_name: pred}
+    return {img_name: "pred"}
